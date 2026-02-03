47332e5e14d0ebceb525977096719738e4852f1b
Mindthemoods <prcrichards@gmail.com>
2026-02-03 17:26:32 -0600

    Add tests for TodoController, remove queryParamAsClass from TodoController

--- server/src/main/java/umm3601/todo/TodoController.java
+++ server/src/main/java/umm3601/todo/TodoController.java
@@ -31,7 +31,7 @@ import io.javalin.http.NotFoundResponse;
 import umm3601.Controller;
 
 /**
- * Controller that manages requests for info about users.
+ * Controller that manages requests for info about todos.
  */
 public class TodoController implements Controller {
 
@@ -39,7 +39,7 @@ public class TodoController implements Controller {
   private static final String API_TODO_BY_ID = "/api/todos/{id}";
   static final String OWNER_KEY = "owner";
   static final String STATUS_KEY = "status";
-  static final String BODY_KEY = "body";
+  static final String BODY_KEY = "body"; //"contains?"
   static final String CATEGORY_KEY = "category";
   static final String LIMIT_KEY = "limit";
   static final String ORDER_KEY = "order";
@@ -140,20 +140,16 @@ public class TodoController implements Controller {
   private Bson constructFilter(Context ctx) {
     List<Bson> filters = new ArrayList<>(); // start with an empty list of filters
     if (ctx.queryParamMap().containsKey(CATEGORY_KEY)) {
-      String category = ctx.queryParamAsClass(CATEGORY_KEY, String.class)
-        .get();
+      String category = ctx.queryParam(CATEGORY_KEY);
       filters.add(eq(CATEGORY_KEY, category));
     }
 
     if (ctx.queryParamMap().containsKey(BODY_KEY)) {
-      String substring = ctx.queryParamAsClass(BODY_KEY, String.class)
-        .get();
+      String substring = ctx.queryParam(BODY_KEY);
       filters.add(regex(BODY_KEY, substring));
-
     }
     if (ctx.queryParamMap().containsKey(OWNER_KEY)) {
-      String owner = ctx.queryParamAsClass(OWNER_KEY, String.class)
-        .get();
+      String owner = ctx.queryParam(OWNER_KEY);
       filters.add(regex(OWNER_KEY, owner));
     }
     if (ctx.queryParamMap().containsKey(STATUS_KEY)) {
